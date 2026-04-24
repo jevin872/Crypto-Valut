21069f3e539d64340efbd0cc7434426ec0c2b81a
jevin872 <vtu24512@veltech.edu.in>
2026-04-24 16:28:16 +0530

    FINAL

--- src/main/java/com/cryptovault/controller/AuthController.java
+++ src/main/java/com/cryptovault/controller/AuthController.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
 
 @Controller
 public class AuthController {
